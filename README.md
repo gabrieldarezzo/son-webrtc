46fcb890c9ad277320540cec60bf900ca1e86ef1
Gabriel <gabrieldarezzo@yahoo.com.br>
2018-12-20 19:45:35 -0200

    Exemplos, e Peer2Peer

---
+++ README.md
@@ -0,0 +1,19 @@
+Install:
+
+cd \xamp\www
+git clone https://github.com/peers/peerjs-server.git
+npm install
+cd peerjs-server
+
+In Windows:
+```
+cd peerjs-server\bin
+node peerjs --port 9000 --key peerjs
+```
+
+
+Caso queira usar a ultima versão do git:
+```
+browserify main.js -o bundle.js
+```
+
--- main.js
+++ main.js
@@ -1,46 +1,86 @@
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.ogGetUserMedia;
-
-if(!navigator.getUserMedia) {
-    console.error('navigator.getUserMedia not Support in your Browser...');
-}
+//var Peer = require('peerjs');
+//var $ = require("jquery");
 
 
-const trackPermissions = {
-    video : true,
-    // video : true,
-};
-
-var video = document.getElementById("localStreamer");
-
-//navigator.getUserMedia(trackPermissions ,handleStream, handleErrorStream)
-
-navigator.mediaDevices
-    .getUserMedia(trackPermissions)
-    .then(handleStream)
-    .catch(handleErrorStream);
-
-
-function handleStream (stream) {
-    var track = stream.getVideoTracks();
-
-    console.log(track)
-    video.srcObject = stream;
-}
-
-function handleErrorStream (err) {
-
-    
-    if(err.name == 'ConstraintNotSatisfiedError') {
-        console.log('invalid Options');    
+(function() {
+    var app = {
+        peers: []
     }
-
-    if(err.name == 'PermissionDinedError' || err.name == 'NotAllowedError') {
-        console.log('Permissio Dinied');    
+    app.checkRTC = function(cb) {
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+        if (!navigator.getUserMedia) {
+            return cb({ err: 'RTC Failed ' }, false)
+        }
+        navigator.getUserMedia({ audio: true, video: true }, cb, function(err) {
+            console.log('CheckRTC => ', err)
+        })
     }
+    app.onRecieveStream = function(stream, name) {
+        var video = $('#' + name)[0]
+        video.src = window.URL.createObjectURL(stream)
+        window.peer_stream = stream
+    }
+    app.newPeer = function() {
+        $("#id").text(app.peer.id)
+    }
+    app.login = function() {
+        var peerID = $('#peer_id').val();
+        if (peerID) {
+            app.peers.push(peerID)
+            con = app.peer.connect(peerID)
+            // con.on('data')
+        }
+    }
+    app.peerConnect = function(con) {
+        app.peer.peer_id = con.peer;
+        app.peer.con = con
+        console.log('A new connection')
+    }
+    app.call = function() {
+        var call  = app.peer.call(app.peer.peer_id, window.localStream)
+        call.on('stream', function(stream) {
+            window.peer_stream = stream
+            app.onRecieveStream(stream, 'pattern')
+        })
+    }
+    app.onReceiveCall = function(call) {
+        call.answer(window.localStream)
+        call.on('stream', function(stream) {
+            window.peer_stream = stream
+            app.onRecieveStream(stream, 'pattern')
+        })
+    }
+    app.peerCall = function(call) {
+        return app.onReceiveCall(call)
+    }
+    app.init = function() {
+
+        
+        /*
+        // Local Settings peerjs-server\bin
+        ///node peerjs --port 9000 --key peerjs
+        const confPeers = {
+            key : 'peerjs',
+            host : 'localhost',
+            port : 9000,
+            path: '/',            
+            debug : 3, // default 0
+        };
+        app.peer = new Peer(confPeers);
+        */
+
+        app.peer = new Peer({ key: 'sdj30v644pfd2t9', debug: 3 })
 
 
-}
-
-
-
-//console.log(navigator.getUserMedia)
+        app.peer.on('open', app.newPeer)
+        app.peer.on('connection', app.peerConnect)
+        app.peer.on('call', app.peerCall)
+        app.checkRTC(function(stream) {
+            window.localStream = stream
+            app.onRecieveStream(stream, 'local')
+        })
+        $("#login").on('click', app.login)
+        $("#call").on('click', app.call)
+    }
+    app.init()
+})();
